c68d4f639c6ec1a9b64fd8b0f2a1b3ac3dd8e16a
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-20 12:04:20 -0500

    installed MUI and Updated Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import NavBar from './components/Navbar.tsx';
+import styled from 'styled-components';
+import cawfee from './assets/cawfee.gif';
+
+const AppContainer = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  background-color: #262626;
+  background-position: top 3rem center;
+  background-image: radial-gradient(50% 50% at top center, rgba(0, 0, 0, .66), #262626), linear-gradient(180deg, #fff, #262626);
+`;
+
+const Container = styled.div`
+  display: flex;
+  height: auto;
+  width: 100vw;
+`;
+
+const Image = styled.img`
+  height: auto;
+  width: 100px;
+`;
 
 function App() {
 
   return (
-    <>
+    <AppContainer>
       <NavBar></NavBar>
-    </>
+      <Container>
+        <Image src={cawfee}/>
+      </Container>
+    </AppContainer>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,57 @@
 import React from "react";
 import styled from "styled-components";
+import { Switch } from "@mui/material";
 
 const NavContainer = styled.div`
-  height: 10rem;
-  width: 100vw;
-  background-color: yellow;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  position: sticky;
+  height: 3rem;
+  background-color: #111;
+  border-bottom: 1px solid white;
+`;
+
+const Name = styled.h3`
+  margin: auto 1rem;
+  color: #edeee8;
+  font-family: sans-serif;
+  font-weight: bolder;
+  font-style: italic;
+`;
+
+const ButtonContainer = styled.nav`
+  display: flex;
+  align-content: flex-end;
+  gap: 2rem;
+  margin: auto;
+`;
+
+const Nav = styled.button`
+  color: #edeee8;
+  margin: auto;
+  font-weight: 500;
+  font-size: 1.25rem;
+  &:hover {
+    position: relative;
+    transform: scale(1.2);
+    cursor: pointer;
+  }
 `;
 
 const Navbar = () => {
-  return (<NavContainer>Navbar</NavContainer>);
+  return (
+    <NavContainer>
+      <Name>Owen Yoshishige</Name>
+      <ButtonContainer>
+        <Nav>Home</Nav>
+        <Nav>About</Nav>
+        <Nav>Work</Nav>
+        <Nav>Contact</Nav>
+      </ButtonContainer>
+      <Switch />
+    </NavContainer>
+  );
 };
 
 export default Navbar;
