ecf81c307e878dc12317fa9fd71e4ff2dc2e2175
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-26 14:50:41 -0500

    Refactored the App component to use MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
 import NavBar from './components/Navbar.tsx';
-import styled from 'styled-components';
-import cawfee from './assets/cawfee.gif';
-
-const AppContainer = styled.div`
-  position: fixed;
-  height: 100vh;
-  width: 100vw;
-  background-color: #262626;
-  background-position: top 3rem center;
-  background-image: radial-gradient(50% 50% at top center, rgba(0, 0, 0, .66), #262626), linear-gradient(180deg, #fff, #262626);
-`;
-
-const Container = styled.div`
-  display: flex;
-  height: auto;
-  width: 100vw;
-`;
-
-const Image = styled.img`
-  height: auto;
-  width: 100px;
-`;
+import Container from "@mui/material/Container";
 
 function App() {
 
   return (
-    <AppContainer>
-      <NavBar></NavBar>
-      <Container>
-        <Image src={cawfee}/>
-      </Container>
-    </AppContainer>
+    <Container disableGutters maxWidth="100vw" sx={{
+      position: "fixed",
+      backgroundColor: "#262626",
+      backgroundPosition: "top 3rem center",
+      backgroundImage: "radial-gradient(50% 50% at top center, rgba(0, 0, 0, .66), #262626), linear-gradient(180deg, #fff, #262626)",
+      height: "100vh"
+    }}>
+      <NavBar/>
+    </Container>
   )
 }
 
