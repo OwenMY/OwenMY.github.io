47f3220d4bceaac3b122231cd2febce60114a91b
Owen Yoshishige <owenyoshishige@gmail.com>
2025-02-26 14:33:12 -0500

    refactored navbar to use MUI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,33 @@
 import React from "react";
-import styled from "styled-components";
-import { Switch } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
 
-const NavContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-content: center;
-  position: sticky;
-  height: 3rem;
-  background-color: #111;
-  border-bottom: 1px solid white;
-`;
-
-const Name = styled.h3`
-  margin: auto 1rem;
-  color: #edeee8;
-  font-family: sans-serif;
-  font-weight: bolder;
-  font-style: italic;
-`;
-
-const ButtonContainer = styled.nav`
-  display: flex;
-  align-content: flex-end;
-  gap: 2rem;
-  margin: auto;
-`;
-
-const Nav = styled.button`
-  color: #edeee8;
-  margin: auto;
-  font-weight: 500;
-  font-size: 1.25rem;
-  &:hover {
-    position: relative;
-    transform: scale(1.2);
-    cursor: pointer;
-  }
-`;
+const sections = ["About", "Work", "Contact"];
 
 const Navbar = () => {
   return (
-    <NavContainer>
-      <Name>Owen Yoshishige</Name>
-      <ButtonContainer>
-        <Nav>Home</Nav>
-        <Nav>About</Nav>
-        <Nav>Work</Nav>
-        <Nav>Contact</Nav>
-      </ButtonContainer>
-      <Switch />
-    </NavContainer>
+    <AppBar
+      position="sticky"
+      sx={{
+        backgroundColor: "#111"
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h6" sx={{fontWeight: "bolder", fontStyle: "italic", fontFamily: 'monospace'}}>
+          Owen Yoshishige
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: "right" } }}>
+          {sections.map(section =>
+            <Button key={section} sx={{ my: 2, color: '#edeee8', display: 'block' }}>{
+              section
+            }</Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
