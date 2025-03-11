a60cf027618b976517658e4da16d2bb49b116bff
Owen Yoshishige <owenyoshishige@gmail.com>
2025-03-11 15:43:21 -0400

    Update Landing Page to be responsive

--- src/sections/Landing.jsx
+++ src/sections/Landing.jsx
@@ -2,17 +2,16 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import owen from "../assets/myself.jpeg";
-import styled from "@emotion/styled";
 
 const Description = () => (
-  <Box sx={{width: "50%", margin: "auto 0 auto 3rem"}}>
+  <Box sx={{width: "50%", margin: "auto 0 auto 3rem", '@media (max-width:750px)': {width: "100%"}}}>
     <Typography
       variant="h5"
       color='#edeee8'
       sx={{
         fontStyle: "italic",
         fontFamily: 'monospace',
-        '@media (max-width:600px)': {
+        '@media (max-width:750px)': {
           fontSize: "1.5rem"
         },
       }}>
@@ -24,7 +23,7 @@ const Description = () => (
       sx={{
         fontWeight: "bolder",
         fontFamily: 'monospace',
-        '@media (max-width:600px)': {
+        '@media (max-width:750px)': {
           fontSize: "2rem"
         },
       }}>
@@ -34,18 +33,17 @@ const Description = () => (
         color='#edeee8'
         sx={{
           fontFamily: 'monospace',
-          '@media (max-width:600px)': {
-            fontSize: "1rem"
-          },
         }}>
           With 2 years of experience, I specialize in React, TypeScript, Java and AWS Services.
       </Typography>
   </Box>
 );
 
-const CatchyAnimation = () => (
-  <Box sx={{width: "50%", margin: "auto 0"}}>
-      <img src={owen}/>
+const Picture = () => (
+  <Box sx={{width: "50%", margin: "auto 0", "@media (max-width:750px)": {display: "none"}}}>
+      <Box sx={{width: "20rem", height: "20rem", margin: "auto"}}>
+        <img src={owen} style={{ width: '100%', height: 'auto', borderRadius: "50%" }}/>
+      </Box>
   </Box>
 );
 
@@ -53,13 +51,13 @@ const Landing = () => {
   return (
     <Container maxWidth="100vw" sx={{
       position: "fixed",
-      height: "100%",
+      height: "80vh",
       display: "flex",
       flexDirection: "row",
       gap: "1rem"
     }}>
       <Description/>
-      <CatchyAnimation />
+      <Picture />
     </Container>
   )
 };
