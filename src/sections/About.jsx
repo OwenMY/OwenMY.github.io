b1ac793d8fabaec101a46eae1f48e2630e6f0278
Owen Yoshishige <owenyoshishige@gmail.com>
2025-03-27 11:01:52 -0400

    Prettied up the About section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,13 +1,31 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
+const Heading = () => (
+  <Box sx={{width: "50%", paddingLeft: "7rem"}}>
+    <Typography variant="h3" sx={{fontWeight: "bolder"}}>About Me</Typography>
+  </Box>
+);
+
+const Description = () => {
+  const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquet eros a justo aliquet rutrum. Pellentesque congue ultrices ullamcorper. Pellentesque placerat eleifend dignissim. Aliquam erat volutpat. Sed vel lorem a enim maximus consectetur. Cras laoreet, purus ac pharetra aliquet, dui nibh lobortis leo, sed ultrices massa justo non ipsum. Maecenas interdum pellentesque mauris, sed euismod mi suscipit at."
+
+  return (
+    <Box sx={{width: "50%", paddingRight: "7rem"}}>
+      <Typography>{description}</Typography>
+    </Box>
+)};
+
 const About = () => {
   return (
     <Box sx={{
       display: "flex",
-      flexDirection: "column"
+      width: "100vw",
+      height: "80vh",
+      margin: "auto"
     }}>
-      <Typography>About Me</Typography>
+      <Heading />
+      <Description />
     </Box>
   );
 };
