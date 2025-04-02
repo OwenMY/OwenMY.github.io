1da66cb8f691267a9722b4b636bbee03428ead5b
Owen Yoshishige <owenyoshishige@gmail.com>
2025-04-02 15:17:05 -0400

    Translated the Contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -6,10 +6,17 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import {FormattedMessage} from "react-intl";
 
 const Heading = () => (
   <Box sx={{width: "100%%", margin: "auto"}}>
-    <Typography variant="h3" sx={{fontWeight: "bold"}}>Contact</Typography>
+    <Typography variant="h3" sx={{fontWeight: "bold"}}>
+      <FormattedMessage
+        id="contact.heading.label"
+        defaultMessage="Contact"
+        description="heading for the contact section"
+      />
+    </Typography>
   </Box>
 );
 
@@ -25,7 +32,11 @@ const CallToAction = () => (
   <Card sx={{ width: "100%", borderRadius: "1rem" }}>
     <CardContent sx={{margin: "auto"}}>
       <Typography variant="overline" sx={{lineHeight: "0.5rem"}}>
-        If you are interested in hiring me or want to collaberate on a business idea, send me a message using the form below. I am looking forward to hearing from you!
+      <FormattedMessage
+        id="contact.calltoaction.description"
+        defaultMessage="If you are interested in hiring me or want to collaberate on a business idea, send me a message using the form below. I am looking forward to hearing from you!"
+        description="a call to action for make people want to contact you"
+      />
       </Typography>
     </CardContent>
   </Card>
@@ -33,11 +44,63 @@ const CallToAction = () => (
 
 const ContactForm = () => (
   <Box component="form" sx={{display: "flex", flexWrap: "wrap", gap: "1rem"}}>
-    <TextField required sx={{width: "17rem"}} label="FirstName" variant="outlined" />
-    <TextField required sx={{width: "17rem"}} label="LastName" variant="outlined" />
-    <TextField required sx={{width: "100%"}} label="Email" variant="outlined" />
-    <TextField required sx={{width: "100%"}} label="Message" multiline minRows={4} maxRows={4} />
-    <Button variant="contained" sx={{width: "100%"}}>Send</Button>
+    <TextField
+      required
+      sx={{width: "17rem"}}
+      label={
+        <FormattedMessage
+          id="contact.input.firstname.label"
+          defaultMessage="FirstName"
+          description="Label indicating that your first name should be written in the input"
+        />
+      }
+      variant="outlined"
+    />
+    <TextField
+      required
+      sx={{width: "17rem"}}
+      label={
+        <FormattedMessage
+          id="contact.input.lastname.label"
+          defaultMessage="LastName"
+          description="Label indicating that your last name should be written in the input"
+        />
+      }
+      variant="outlined"
+    />
+    <TextField
+      required
+      sx={{width: "100%"}}
+      label={
+        <FormattedMessage
+          id="contact.input.email.label"
+          defaultMessage="Email"
+          description="Label indicating that your last name should be written in the input"
+        />
+      }
+      variant="outlined"
+    />
+    <TextField
+      required
+      sx={{width: "100%"}}
+      label={
+        <FormattedMessage
+          id="contact.input.message.label"
+          defaultMessage="Message"
+          description="Label indicating that your message should be written in the input"
+        />
+      }
+      multiline
+      minRows={4}
+      maxRows={4}
+    />
+    <Button variant="contained" sx={{width: "100%"}}>
+      <FormattedMessage
+        id="contact.submit.button.label"
+        defaultMessage="Send"
+        description="Label indicating that pressing the button will submit the form"
+      />
+    </Button>
   </Box>
 );
 
